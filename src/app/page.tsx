22efad3a6f7ef24e384c30a1b2782654b5638714
Borjan Kalinoski <37346995+BorjanKalinoski@users.noreply.github.com>
2023-05-29 11:31:44 +0200

    Make cv into svg format (#3)
    * test if cspell will throw an error when a issue is found
    
    * make-cv-into-svg-format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,10 @@
-"use client"
-import {useWindowSize} from "~/hooks/use-window-size";
-import {Pdf} from "~/components/pdf";
-import {useState} from "react";
-import {LoadingSpinner} from "~/components/loading-spinner";
+import {Cv} from "~/components/cv";
 
 
 function App() {
-    const [isLoadingPdf, setIsLoadingPdf] = useState(true);
-
-    const {
-        width,
-        height
-    } = useWindowSize();
-
-    const cvWidth = width && width > 1024 ? undefined : width;
-    const cvHeight = height || 500;
-
-
     return (
         <div className="w-[100%] h-[100vh] flex justify-center items-center bg-slate-50">
-            <Pdf
-                width={cvWidth} height={cvHeight}
-                src="/borjan-kalinoski-cv.pdf"
-                className="shadow-2xl"
-                onLoadSuccess={() => {
-                    setIsLoadingPdf(false);
-                }}
-            />
-
-            {isLoadingPdf && <LoadingSpinner/>}
+            <Cv />
         </div>
     );
 }
